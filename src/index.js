decd7a9eff174ca997fbb94923bdf59274d0481d
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-09-21 10:44:29 +1200

    created animations for paddle game

---
+++ src/index.js
@@ -0,0 +1,41 @@
+//TODO - 
+
+/**
+ 1. Create Canvas Context
+ 2. Create and draw ball
+ 3. Create a Paddle
+ 4. Create bricks
+ 5. Create score window
+ 6. Add Update Function
+ 7. Rules Functions
+ 8. Rules for the animation
+ 9. Move paddle 
+ 10. Keyboard events with the paddle
+ 11. Moving ball
+ 12. Setup Wall Boundaries
+ 13. Score Updates
+ 14. When the ball break the bricks
+ 15. Lose- redrew the bricks and reset the score.
+ * 
+ */
+
+ let score = 0 //setting out scoreboard to zero 
+
+ const brickRowCount = 9 
+ const brickColumnCount = 5
+
+ const ball = {}
+
+ // Create Paddle 
+
+ const paddle = {}
+
+ // Create Brick 
+
+ const brickInfo = {}
+
+ // Create brick array 
+
+ const bricks = []
+
+ 
